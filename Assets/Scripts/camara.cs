1740453f6bf74299f26e0c2b342a6896e014018c
Ana Larisa <89612091+LariLapiz@users.noreply.github.com>
2022-12-19 19:06:23 -0600

    escenario_1_Armado

--- Assets/Scripts/camara.cs
+++ Assets/Scripts/camara.cs
@@ -2,22 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class camara : MonoBehaviour
+public class Camara : MonoBehaviour
 {
 
-    public GameObject player;
-    public Vector2 minimo;
-    public Vector2 maximo;
-    public float suavizado;
-    Vector2 velocidad;
+
+	public Player PosicionPlayer;
+	Vector3 SiguientePosicion;
+
+	float DistanciaMover;
+
+	// Use this for initialization
+	void Start()
+	{
+		PosicionPlayer = FindObjectOfType<Player>();
+		SiguientePosicion = PosicionPlayer.transform.position;
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		DistanciaMover = PosicionPlayer.transform.position.x - SiguientePosicion.x;
+		transform.position = new Vector3(transform.position.x + DistanciaMover, transform.position.y, transform.position.z);
+		SiguientePosicion = PosicionPlayer.transform.position;
 
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocidad.x, suavizado);
-        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocidad.y, suavizado);
-
-        transform.position = new Vector3(Mathf.Clamp(posX, minimo.x, maximo.x), Mathf.Clamp(posY, minimo.y, maximo.y), transform.position.z);
-    }
+	}
 }
